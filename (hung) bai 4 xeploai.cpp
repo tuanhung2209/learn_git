28b95387fd00305352c31c5fc55805a28b44b111
unknown <tuanhung220901@gmail.com>
2019-12-23 00:15:39 +0700

    initialize commit

---
+++ (hung) bai 4 xeploai.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+			float n;
+			scanf ("%f",&n);
+		if ((n<=10)&&(n>=8))
+			{printf("gioi");}
+		
+		else if ((n<8)&&(n>=6))
+			{printf("kha");}
+		
+		else if ((n<6)&&(n>=4))
+			{printf ("trung binh");}
+		
+		else if ((n<4)&&(n>0))
+			{printf("yeu");}
+		
+	return 0;
+}
